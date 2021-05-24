d9b37834d4840b22752abae4ee844bcf21c3616f
Santiago SR <santisachezromero@gmail.com>
2021-05-24 12:59:23 -0300

    fix(reviewer fix): fix in html, css and js

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,24 @@
-//open and close modal
 const profileEditButtonEl = document.querySelector(".profile__edit-button");
 const profileCloseButtonEl = document.querySelector(".modal__close-button");
 const modalEl = document.querySelector(".modal");
-
-function closeModal() {
-  modalEl.classList.remove("modal_open");
-}
-
-profileEditButtonEl.addEventListener("click", function () {
-  modalEl.classList.add("modal_open");
-});
-
-profileCloseButtonEl.addEventListener("click", function () {
-  closeModal();
-});
-
-//form edition
 const editProfileEl = document.querySelector(".modal__form");
-
 const editProfileNameInput = document.querySelector(
   ".modal__form-item_type_name"
 );
 const profileNameEl = document.querySelector(".profile__name");
-
 const editProfileAboutInput = document.querySelector(
   ".modal__form-item_type_about"
 );
 const profileAboutEl = document.querySelector(".profile__about");
 
-editProfileNameInput.value = profileNameEl.textContent;
-editProfileAboutInput.value = profileAboutEl.textContent;
+function openForm() {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileAboutInput.value = profileAboutEl.textContent;
+}
+
+function closeModal() {
+  modalEl.classList.remove("modal_open");
+}
 
 function submitForm(event) {
   event.preventDefault();
@@ -37,5 +26,14 @@ function submitForm(event) {
   profileAboutEl.textContent = editProfileAboutInput.value;
   closeModal();
 }
+
+profileEditButtonEl.addEventListener("click", function () {
+  modalEl.classList.add("modal_open");
+  openForm();
+});
+
+profileCloseButtonEl.addEventListener("click", function () {
+  closeModal();
+});
 
 editProfileEl.addEventListener("submit", submitForm);
